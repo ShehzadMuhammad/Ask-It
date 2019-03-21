13f6d55989f658f9c1e388aae6bed92e69a679ef
Shehzad Muhammad <shehzad.muhammad@outlook.com>
2019-03-20 22:14:54 -0400

    removed some warnings

--- client/src/components/MainPage/AddPostDialog.js
+++ client/src/components/MainPage/AddPostDialog.js
@@ -10,37 +10,10 @@ import FormControl from '@material-ui/core/FormControl';
 
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
 
 
 import { connect } from 'react-redux';
 import { addPost } from '../../actions/postActions';
-
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
 
 class AddPostDialog extends React.Component {
     constructor(props){
@@ -76,7 +49,6 @@ class AddPostDialog extends React.Component {
 
   render() {
     const { fullScreen } = this.props;
-    const { classes } = this.props;
     return (
       <div>
         <Button variant="outlined" align="center" color="inherit" paddingbottom="20px" onClick={this.toggle}>

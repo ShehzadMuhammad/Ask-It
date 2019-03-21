0414b2297270edc9b2076b87e94c9078a7a92737
Shehzad Muhammad <shehzad.muhammad@outlook.com>
2019-03-21 13:51:03 -0400

    added a path for production build

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import forum from './api/routes/forumRoutes';
 
 const passport = require("passport");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -31,6 +32,17 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
+
+//Server Static assets if in production
+if(process.env.NODE_ENV === 'production'){
+	//Set Static Folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+
 // Routes
 app.use("/api", forum);
 
